e1cfb5123d2663ae50369195cdc942c90b95f032
Anna Wilson <anna.elisabeth@gmail.com>
2024-02-29 13:07:06 +0000

    nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import megaCat from './megaCat.PNG';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+
+
+// import './App.css';
 import Home from './Components/ItemFolder/HomeFolder/Home';
 import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Items from './Components/ItemFolder/Items';
@@ -7,46 +11,48 @@ import Items from './Components/ItemFolder/Items';
 function App() {
   return (
 
-    <header>
   
+    
     <Router>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only"></span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Items</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">AboutUs</a>
-      </li>
+      
+      <div>
+     
+        
+  <nav className="navbar navbar-expand-sm bg-light justify-content-center" style={{ backgroundColor: "#7CB9E8" }}>
+      
+      <img src={megaCat} alt='Logo Icon' className="navbar-brand" width="80px" />
+  
+    
 
-    </ul>
-  </div>
-</nav>
+      <ul className="navbar-nav mx-auto">
+        <li className="nav-item">
+          <Link className="nav-link" style={{ color: "#00308F" }} to='/'>Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" style={{ color: "#00308F" }} to='/items'>Items</Link>
+        </li>
+      </ul>
+  
+
+        </nav> 
 
 
       <Routes>
 
         <Route path="/" element={<Home/>} />
 
-        <Route path="/Items" element={<Items/>} /> 
+        <Route path="/items" element={<Items/>} /> 
 
-        {/* <Route path="/Cart" element={<Cart/>} />
+        {/*  <Route path="/Cart" element={<Cart/>} /> */}
 
-        <Route path="/About us" element={<AboutUs/>} />  */}
 
       </Routes>
 
+
+</div>
     </Router>
 
-  </header>
+  
 
 
 
--- src/Components/ItemFolder/HomeFolder/Home.jsx
+++ src/Components/ItemFolder/HomeFolder/Home.jsx
@@ -1,10 +1,14 @@
+// import stockRoom from './stockRoom';
+
 function Home(){
 
     return(
+
         <div>
+        {/* // <div style={{ backgroundImage: `url(${stockRoom})` }}> */}
 
 <h1>Welcome to stock management</h1>
-<h1>LOG IN TO PAGE GOES HERE! - YES IT DOES</h1>
+<h1>LOG IN PAGE GOES HERE! - YES IT DOES</h1>
 <p>Make your purchase order dreams come true</p>
 
 
